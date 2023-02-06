87ad931b0d103e153fd5a8de3fd6d6989a9c634a
Ivan Spahiyski <ispahiyski@gmail.com>
2023-02-06 15:53:58 +0200

    Code clean up and comments.

--- FolderWatcherWindowsServiceAdmin/FolderWatcherWindowsServiceAdmin.cs
+++ FolderWatcherWindowsServiceAdmin/FolderWatcherWindowsServiceAdmin.cs
@@ -1,40 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FolderWatcherWindowsServiceAdmin
 {
     public partial class FolderWatcherWindowsServiceAdmin : Form
     {
+        // service management
+        private ServiceController serviceController;
 
-        ServiceController serviceController;
+        // service status
+        private ServiceControllerStatus serviceControllerStatus;
 
-        ServiceControllerStatus serviceControllerStatus;
-
-        enum btnStartStopText
+        // button text
+        private enum btnStartStopText
         {
             Start,
             Stop
         }
-        
+
         public FolderWatcherWindowsServiceAdmin()
         {
+            // create ServiceController instance for FolderWatcherWindowsService
             serviceController = new ServiceController("FolderWatcherWindowsService");
+
             InitializeComponent();
         }
 
+        /// <summary>
+        /// On form load.
+        /// </summary>
+        /// <param name="sender">form</param>
+        /// <param name="e">event arguments</param>
         private void FolderWatcherWindowsServiceAdmin_Load(object sender, EventArgs e)
         {
             UpdateUI();
         }
 
+        /// <summary>
+        /// On form closing.
+        /// </summary>
+        /// <param name="sender">form</param>
+        /// <param name="e">event arguments</param>
         private void FolderWatcherWindowsServiceAdmin_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (serviceController != null)
@@ -48,12 +55,20 @@ namespace FolderWatcherWindowsServiceAdmin
         /// </summary>
         private void UpdateUI()
         {
+            // get status
             serviceControllerStatus = serviceController.Status;
+            // set lblStatus text
             lblStatus.Text = serviceControllerStatus.ToString();
+            // set btnText text
             string btnText = serviceControllerStatus == ServiceControllerStatus.Running ? btnStartStopText.Stop.ToString() : btnStartStopText.Start.ToString();
             btnChangeStatus.Text = btnText;
         }
 
+        /// <summary>
+        /// btnChangeStatus click.
+        /// </summary>
+        /// <param name="sender">btnChangeStatus</param>
+        /// <param name="e">event arguments</param>
         private void btnChangeStatus_Click(object sender, EventArgs e)
         {
             if (serviceControllerStatus == ServiceControllerStatus.Running)
